af4a85a58c7e0ff5d91910f8fabd0b6b42bb001a
ndom91 <yo@ndo.dev>
2022-09-03 23:37:54 +0200

    fix: refactor shortcutCard

--- src/components/shortcutCard.jsx
+++ src/components/shortcutCard.jsx
@@ -1,6 +1,19 @@
 import { Badge } from '@nextui-org/react'
 
+const storyTypeIcons = {
+  bug: '🐛',
+  feature: '🚧',
+}
+
 export default function ShortcutCard({ story, workflows, epics }) {
+  const workflow = workflows
+    .find((wf) => wf.id === story.workflow_id)
+    ?.name.slice(0, 3)
+
+  const workflowState = workflows
+    .find((wf) => wf.id === story.workflow_id)
+    ?.states.find((state) => state.id === story.workflow_state_id)?.name
+
   return (
     <li key={story.id} className="m-0 p-0">
       <a
@@ -9,46 +22,39 @@ export default function ShortcutCard({ story, workflows, epics }) {
         rel="noopener noreferer noreferrer"
         className="flex flex-col items-start justify-start rounded-md p-2 hover:cursor-pointer hover:bg-gray-800"
       >
-        <span className="flex w-full items-center justify-start gap-2 text-lg font-extralight">
-          <Badge
-            variant="flat"
-            color="primary"
-            size="md"
-            disableOutline
-            className="w-14"
-          >
-            {story.id}
-          </Badge>
-          <div className="flex flex-grow flex-col items-start justify-center">
-            {story.epic_id ? (
-              <span className="font-mono text-sm font-light text-slate-400">
-                {epics.find((epic) => epic.id === story.epic_id).name}
-              </span>
-            ) : null}
-            <span className="">{story.name}</span>
-            <div className="flex items-center justify-start">
-              <Badge
-                variant="flat"
-                color="primary"
-                size="md"
-                disableOutline
-                className="mr-2 text-sm uppercase text-slate-300"
-              >
-                {story.story_type.charAt(0)}
-              </Badge>
-              <span className="mr-2 text-sm text-slate-300">
-                {workflows.find((wf) => wf.id === story.workflow_id)?.name} -{' '}
-                {
-                  workflows
-                    .find((wf) => wf.id === story.workflow_id)
-                    ?.states.find(
-                      (state) => state.id === story.workflow_state_id
-                    )?.name
-                }
-              </span>
+        <div className="flex flex-grow flex-col items-start justify-center space-y-1">
+          {story.epic_id ? (
+            <div className="text-sm font-light text-slate-400">
+              {epics.find((epic) => epic.id === story.epic_id).name}
             </div>
+          ) : null}
+          <div className="text-lg font-extralight">{story.name}</div>
+          <div className="flex items-center justify-start space-x-2">
+            <Badge variant="flat" color="primary" size="sm" disableOutline>
+              {story.id}
+            </Badge>
+            <Badge
+              variant="flat"
+              color="primary"
+              size="sm"
+              disableOutline
+              className="text-sm uppercase text-slate-300"
+              title={story.story_type}
+            >
+              {storyTypeIcons[story.story_type]}
+            </Badge>
+            <Badge
+              variant="flat"
+              color="secondary"
+              size="sm"
+              disableOutline
+              title={`${workflow} - ${workflowState}`}
+              className="font-extralight"
+            >
+              {workflow} - {workflowState}
+            </Badge>
           </div>
-        </span>
+        </div>
       </a>
     </li>
   )
